71d87ce314555e4253b7e58bd9bdb841a5f4f8bf
leiming <leimingbao@hotmail.com>
2020-05-20 20:19:54 +0800

    WIP

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.myapplication
 
 import android.app.Activity
 import android.os.Bundle
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : Activity() {
 
@@ -10,7 +9,5 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val recel = findViewById<RecyclerView>(R.id.recycle)
     }
 }
